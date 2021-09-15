0e01a9f8f4e82dda2f9a7b02b149b51631a0e68a
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-14 21:06:06 -0300

    created Request struct and array of requests. created restricted modifier for only manager functions

--- Campaign.sol
+++ Campaign.sol
@@ -1,10 +1,22 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
     
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
     
     function Campaign(uint minimum) public {
         manager = msg.sender;
@@ -15,4 +27,5 @@ contract Campaign {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
     }
+    
 }
